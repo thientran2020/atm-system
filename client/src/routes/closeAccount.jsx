07ec547995ee94c20f5515dda010ec2c902051fe
nhattuannguyensjsu <100452313+nhattuannguyensjsu@users.noreply.github.com>
2022-04-21 11:09:49 -0700

    Add files via upload

--- client/src/routes/closeAccount.jsx
+++ client/src/routes/closeAccount.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import NavBar from '../components/navbar';
 import Foot from '../components/footer';
-export default function closeAccount() {
-    return (
 
-        <div>
-        <header className="App-header">
+function closeAccount() {
+    return (
+      
+  <div>
+      <header className="App-header">
          <h1> ATM SYSTEM PROJECT </h1>
          <NavBar />
-       </header>
-       <body  className="profilex" >
-  
+      </header>
+       
+
+      <body  className="profilex" >
+        <div class="container">
+         <table class="table">
+         <thead id="thead" style="background-color: #26a2af">
+         <tr>
+             <th scope="col">Account Number</th>
+             <th scope="col">Balance</th>
+             <th scope="col">Account Type</th>
+             <th scope="col"> Action </th>
+         </tr>
+         </thead>
  
-<h1>closeAccount</h1>
- 
-     </body>
+         <tr>
+         /* Link to database and display account info here (account_id, balance, account_type)
+             <th>Account Number data</th>
+             <th>Balance data</th>
+             <th>Account Type data</th>
+             <th>
+                 <a href='#' type='button' class="btn btn-danger"> Close </a>
+
+             </th>
+            <br></br>
+            /* Function for CLOSE button to ask to reenter password for verification before close account */
+            /* If balance > 0, ask user if they want to get a check or transfer to another account */
+         </tr>
+
+        </table>
+        </div>
+      </body>
      <footer>
    <Foot />
  </footer> 
@@ -26,3 +52,5 @@ export default function closeAccount() {
         
     )
 }
+
+export default closeAccount;
